028425ed9654cacd125d857bb006007da80c2ab2
darthVader <ashiralvi916@gmail.com>
2023-08-21 11:30:21 -0700

    Making the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -93,6 +93,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         quantity    :     1
       });
     }
-    console.log(cart);
+
+    // Here we calculate cart quantity looping through the cart array and 
+    //saving cart quantity and displaying it onto the webpage using DOM
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    
   });
 });
