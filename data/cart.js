72fb9235a37afb7d0ec04ffa5e09d14b79f73a68
darthVader <ashiralvi916@gmail.com>
2023-08-22 10:17:30 -0700

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -4,14 +4,23 @@
 //import
 
 // We are creating default value for the checkout page here
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity : 2
-},
-{
-  productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity  : 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart =  [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity : 2
+  },
+  {
+    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity  : 1
+  }];
+  
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Making add to cart button reactive using DOM by adding an eventlistener
 // This applies to all the add-to-cart button
@@ -36,6 +45,7 @@ export function addToCart(productId){
       quantity    :     1
     });
   }
+  saveToStorage();
 };
 
 export function removeFromCart(productId){
@@ -46,4 +56,5 @@ export function removeFromCart(productId){
     }
   });
   cart = newCart
+  saveToStorage();
 }
